211b3d4a0a3ca640f158ea16be11ac80ad8b6d10
Ankit-Suman <ankitsuman318@gmail.com>
2021-06-12 14:48:11 +0530

    Updated Angular

--- MedicalReportBook/MedicalReportBookAngularClient/src/app/app.module.ts
+++ MedicalReportBook/MedicalReportBookAngularClient/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LandingComponent } from './landing/landing.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginService } from './login.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [LoginService],
+  bootstrap: [LoginComponent]
 })
 export class AppModule { }
---
+++ MedicalReportBook/MedicalReportBookAngularClient/src/app/register.ts
@@ -0,0 +1,4 @@
+export class Register {
+    constructor(public UserName:string ,public LoginName:string,public Password:string ,
+    public Email:string ,public ContactNo:string,public Address:string ){}
+}

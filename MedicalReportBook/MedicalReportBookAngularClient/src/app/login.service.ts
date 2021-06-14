11c90263640112f0133de097681c1852a7db12fe
vishalrjv <vishalrj557@gmail.com>
2021-06-14 14:11:53 +0530

    angular minor changes

--- MedicalReportBook/MedicalReportBookAngularClient/src/app/login.service.ts
+++ MedicalReportBook/MedicalReportBookAngularClient/src/app/login.service.ts
@@ -1,31 +1,31 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';  
-import {HttpHeaders} from '@angular/common/http';  
-import { from, Observable } from 'rxjs';  
-import { Register } from "../app/register";  
+import {HttpClient} from '@angular/common/http';
+import {HttpHeaders} from '@angular/common/http';
+import { from, Observable } from 'rxjs';
+import { Register } from "../app/register";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  Url :string;  
-  token: string='';  
-  header : any;  
-  constructor(private http : HttpClient) {   
-  
-    this.Url = 'http://localhost:57071';  
-  
-    const headerSettings: {[name: string]: string | string[]; } = {};  
-    this.header = new HttpHeaders(headerSettings);  
-  }  
-  Login(model : any){  
-    debugger;  
-     var a =this.Url+'/api/AppUser/Login';  
-   return this.http.post<any>(this.Url+'/api/AppUser/Login',model,{ headers: this.header});  
-  }  
-   CreateUser(register:Register)  
-   {  
-    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };  
-    return this.http.post<Register[]>(this.Url + '/api/AppUser/Registration', register, httpOptions)  
-   }  
+  Url :string;
+  token: string='';
+  header : any;
+  constructor(private http : HttpClient) {
+
+    this.Url = 'http://localhost:57071/api/AppUser/';
+
+    const headerSettings: {[name: string]: string | string[]; } = {};
+    this.header = new HttpHeaders(headerSettings);
+  }
+  Login(model : any){
+    debugger;
+     var a =this.Url+'Login';
+   return this.http.post<any>(a,model,{ headers: this.header});
+  }
+   CreateUser(register:Register)
+   {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<Register[]>(this.Url + 'Registration', register, httpOptions)
+   }
 }

377b03cd0f123bb0febed70674db70af03aeed17
Ankit-Suman <ankitsuman318@gmail.com>
2021-06-17 16:06:24 +0530

    TestView

--- MedicalReportBook/MedicalReportBookAngularClient/src/app/ptest-report/ptest-report.component.ts
+++ MedicalReportBook/MedicalReportBookAngularClient/src/app/ptest-report/ptest-report.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { TestReport } from '../Source/test-report';
-
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-ptest-report',
   templateUrl: './ptest-report.component.html',
@@ -9,23 +9,40 @@ import { TestReport } from '../Source/test-report';
 })
 export class PtestReportComponent implements OnInit {
   treports:TestReport[] = [];
+  formData:any;
   constructor(private http :HttpClient) {
   
   }
+  public url:string="http://localhost:57071/api/Patient/ViewLabReport/";
+
+  public urlt:string="";
+
+  public UserId=localStorage.getItem('UId');
 
   ngOnInit(): void {
-    this.http.get('#').subscribe(
-      (data)=>{this.treports= data as TestReport[];},
-      (err)=>{
-        if(err.status===404){
-          alert('Api not available');
-        }
-        else if(err.status===500){
-          alert('Server Error');
-        }
-      }
+    this.formData=new FormGroup({
+      name    : new FormControl()
+    });
 
-    );
   }
 
-}
+    ontestsubmit(data: { TestName: string; }){ 
+      this.urlt= this.url+this.UserId+'/'+data.TestName;
+ 
+      this.http.get(this.urlt).subscribe(
+       (data)=>{this.treports= data as TestReport[];},
+       (err)=>{
+         if(err.status===404){
+           alert('Api not available');
+         }
+         else if(err.status===500){
+           alert('Server Error');
+         }
+       }
+
+      );
+    }
+  }
+  
+
+
